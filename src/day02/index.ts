2ddf7dad98b1c3c2128bb33e71374e3b9278606c
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2022-12-02 17:56:41 +0100

    Day02: Use index

--- src/day02/index.ts
+++ src/day02/index.ts
@@ -31,9 +31,15 @@ export const rockPaperScissors01 = async () => {
 
   // O(n)
   for (const s of lines) {
-    const [a, b] = s.split(' ');
+    const a = s[0], b = s[2];
     // O(k)
-    score += points[b] + (winsTo[a] == b ? 0 : equalsTo[a] == b ? 3 : 6);
+    score += points[b] + (
+      // We've lose
+      winsTo[a] == b ? 0 
+      // Draw
+      : equalsTo[a] == b ? 3 
+      // Win
+      : 6);
   }
 
   return score;
@@ -46,7 +52,7 @@ export const rockPaperScissors02 = async () => {
   let score = 0;
   // O(n)
   for (const s of lines) {
-    const [a, res] = s.split(' ');
+    const a = s[0], res = s[2];
     // O(k)
     const b = res == 'X' ? winsTo[a] : res == 'Y' ? equalsTo[a] : losesTo[a];
     // O(k)
